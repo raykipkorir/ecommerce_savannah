922f1399730b54ffd7067984966f8ff2507e5b6f
Raymond Kipkorir <lagatraymondkipkorir@gmail.com>
2024-02-19 09:17:35 +0300

    update settings.py

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config("SECRET_KEY", default="my_super_secret_key")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config("DEBUG", default=False)
+DEBUG = config("DEBUG", default=False, cast=bool)
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='*', cast=Csv())
 
