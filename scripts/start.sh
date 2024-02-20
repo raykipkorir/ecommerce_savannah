daa0f3fe266d758119ab6768f2c3c9aef83c5265
Raymond Kipkorir <lagatraymondkipkorir@gmail.com>
2024-02-20 05:51:04 +0300

    Update gunicorn worker timeout

--- scripts/start.sh
+++ scripts/start.sh
@@ -7,4 +7,4 @@ python manage.py migrate
 
 python manage.py collectstatic --noinput
 
-gunicorn ecommerce.wsgi -t 120 -keep-alive 120 -b 0.0.0.0:8000
+gunicorn ecommerce.wsgi -t 120 --keep-alive 120 -b 0.0.0.0:8000
