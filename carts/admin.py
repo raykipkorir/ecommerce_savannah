ae50966aed4d00ef6c5cd02464fdd01acb7d8b5d
Raymond Kipkorir <lagatraymondkipkorir@gmail.com>
2024-02-24 10:45:56 +0300

    Customize admin panel

--- carts/admin.py
+++ carts/admin.py
@@ -2,5 +2,9 @@ from django.contrib import admin
 
 from .models import Cart, CartItem
 
+class CartItemAdmin(admin.ModelAdmin):
+    list_display = ["cart", "product", "quantity"]
+
+
+admin.site.register(CartItem, CartItemAdmin)
 admin.site.register(Cart)
-admin.site.register(CartItem)
